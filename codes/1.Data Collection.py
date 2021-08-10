39b79903dec334f4ce5006114012321519c98a68
sai-2811 <saitejapasula@gamil.com>
2021-08-09 21:02:01 -0400

    Wiki Data

--- codes/1.Data Collection.py
+++ codes/1.Data Collection.py
@@ -1,3 +1,15 @@
+# Import the libraries
 import pandas as pd
+import os
+
+# Change the directory
+os.chdir("C:/Users/saite/PycharmProjects/py38/0.GH/Chicago-Bears-Analysis")
+
+# Pull the win-loss ratio table
+win_los_wiki = 'https://en.wikipedia.org/wiki/Super_Bowl'
+win_los_info = pd.read_html(win_los_wiki,header=0)
+win_los_tm = pd.DataFrame(win_los_info[4])
+win_los_tm.to_csv("./data/win_los_tm.csv",header=True)
+
+# Chicago Bears Data
 url = 'https://www.chicagobears.com/team/players-roster/'
-dfs = pd.read_html(url,header=0)
